41a040dde35e6c2a99412b15f24b09ccf14ca38b
ParimalMuley <muleyparimal33@gmail.com>
2026-03-16 19:05:48 +0000

    changed gcs pull script

--- scripts/pull_gcs.py
+++ scripts/pull_gcs.py
@@ -1,21 +1,20 @@
 import os
 from google.cloud import storage
-
+ 
 bucket_name = os.environ["GCS_BUCKET"]
 object_path = os.environ["GCS_OBJECT_PATH"]
 artifacts   = os.environ["ARTIFACTS_DIR"]
-creds_file  = os.environ["GOOGLE_CREDENTIALS"]
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds_file
-
+ 
+# google-cloud-storage picks up credentials automatically from the
+# GCE metadata server when running on a GCP VM.
 client   = storage.Client()
 blob     = client.bucket(bucket_name).blob(object_path)
 filename = os.path.basename(object_path)
 dest     = os.path.join(artifacts, filename)
-
+ 
 print(f"Downloading gs://{bucket_name}/{object_path} -> {dest}")
 blob.download_to_filename(dest)
 print(f"Done: {filename} ({os.path.getsize(dest):,} bytes)")
-
+ 
 with open(os.path.join(artifacts, ".downloaded_file"), "w") as f:
     f.write(filename)

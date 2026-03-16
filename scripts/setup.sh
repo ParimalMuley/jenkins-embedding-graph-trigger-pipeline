f47b30c89fa48c115c74016b64e950bb6a5b5014
ParimalMuley <muleyparimal33@gmail.com>
2026-03-16 18:24:19 +0000

    changed setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-echo "Creating virtual environment"
+ 
+echo "Creating virtual environment..."
 mkdir -p "${ARTIFACTS_DIR}"
 python3 -m venv "${PYTHON_VENV}"
 . "${PYTHON_VENV}/bin/activate"
-
-echo "Installing dependencies"
+ 
+echo "Installing Python dependencies..."
 pip install --quiet --upgrade pip
 pip install --quiet -r requirements.txt
-
+ 
 echo "Setup complete."
